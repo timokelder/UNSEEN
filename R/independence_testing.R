f5b25f3ecc5dce26426cbfa0c79b242267b5ac73
timokelder <timo.kelder@gmail.com>
2020-08-11 14:22:58 +0200

    added tests

---
+++ R/independence_testing.R
@@ -0,0 +1,93 @@
+#' Calculate the correlation between each unique pair of ensemble members.
+#'
+#' @param ensemble The UNSEEN ensemble. This function expects a dataframe with the columns variable (precipitation), ensemble (number), leadtime.
+#' It expects that the first member is 0 (as used in CDS) and that the first lead time is 2
+#' (first month shows dependence and is therefore removed). See for example the SEAS5_UK dataset using  `?SEAS5_UK`
+#' @param n_ensembles The number of ensemble members (not including pooled leadtimes) in the UNSEEN ensemble. Defaults to 25.
+#' @param n_lds The number of leadtimes in the UNSEEN ensemble. Defaults to 5.
+#' @param var_name The column name containing the variable to be analyzed. Defaults to "tprate".
+#' @param ens_name The column name containing the ensemble members. Defaults to "number".
+#' @param ld_name The column name containing the leadtimes. Defaults to "leadtime".
+#' @return A plot showing the boxplot of the pairwise correlations for each leadtime. See Kelder et al. 2020.
+#' @export
+#' @section Warning:
+#' The confidence intervals are based on a bootstrap test with n_ensembles = 25. These are now pre-computed and therefore
+#' will not give the right CI bounds for different ensemble sizes. The bootstrapping test is included in the documentation in data_raw, but
+#' might be included in the future as a function of n_ensembles and n_lds.
+independence_test <- function(ensemble, n_ensembles = 25, n_lds = 5, var_name = "tprate", ens_name = "number", ld_name = "leadtime") {
+  `2.5%` <- `97.5%` <- Boxstat <- Freq <- leadtime <- x <- NULL
+
+
+  ## Perform the pairwise test
+  correlations_lds <- array( # create and array to fill the correlations in
+    dim = c(n_ensembles, n_ensembles, n_lds), # Dimensions
+    dimnames = list(
+      memberx = as.character(0:(n_ensembles - 1)), # The names of the members start at 0
+      membery = as.character(0:(n_ensembles - 1)), # Lead times start at 2 (first month shows dependence and is therefore removed)
+      leadtime = as.character(2:(n_lds + 1))
+    )
+  )
+  # lds, mbmrs
+  for (ld in 2:(n_lds + 1)) {
+    for (mbr1 in 0:(n_ensembles - 1)) {
+      for (mbr2 in 0:(n_ensembles - 1)) {
+        if (mbr1 > mbr2) { ## Only calculate this for the top half of the correlation matrix, as not to duplicate any values
+          ## -> avoid correlating mbr 1 with mbr2 and then mbr2 with mbr 1.
+          predictant <- ensemble[[var_name]][ensemble[[ens_name]] == mbr1 & ensemble[[ld_name]] == ld]
+          predictor <- ensemble[[var_name]][ensemble[[ens_name]] == mbr2 & ensemble[[ld_name]] == ld]
+
+          correlations_lds[as.character(mbr1), as.character(mbr2), as.character(ld)] <- stats::cor(
+            x = predictant,
+            y = predictor,
+            method = "spearman"
+          )
+        }
+      }
+    }
+  }
+
+  ##Convert to dataframe for ggplot
+  correlations_lds_df = as.data.frame(as.table(correlations_lds))
+  ## And plot
+  CI_bounds_boxplots_df = as.data.frame(t(CI_bounds_boxplots))
+  CI_bounds_boxplots_df$Boxstat = c('lower whisker', 'lower IQR', 'median','upper IQR', 'upper whisker')
+  CI_bounds_boxplots_df_ld2 <- CI_bounds_boxplots_df
+  CI_bounds_boxplots_df_ld2$x <- 0.5
+  CI_bounds_boxplots_df_ld_end <- CI_bounds_boxplots_df
+  CI_bounds_boxplots_df_ld_end$x <- n_lds + .5
+  CI_bounds_boxplots_df_all = rbind(CI_bounds_boxplots_df_ld2,CI_bounds_boxplots_df_ld_end)
+
+  ggplot2::ggplot(data = correlations_lds_df) +
+    ggplot2::geom_violin(
+      mapping = ggplot2::aes(
+        x = leadtime,
+        y = Freq,
+        group = leadtime
+      )
+    ) +
+    ggplot2::geom_boxplot(mapping = ggplot2::aes(x = leadtime, y = Freq,group = leadtime), width=0.2) + # same as violin
+    ggplot2::geom_ribbon(data=CI_bounds_boxplots_df_all,
+                         ggplot2::aes(x = x,
+                                      ymin = `2.5%`,ymax=`97.5%`,
+                                      group = Boxstat),
+                         fill="grey",
+                         alpha="0.4") +
+    ggplot2::theme_classic() +
+    ggplot2::ylab(bquote("Spearman" ~ rho))
+
+
+  # The old plot with base R
+  #   # par(mfrow=c(1,2),mar=c(4.5,5.1,2.1,2.1),cex.axis=1.5, cex.lab=1.5,cex.main=1.5)
+  #   boxplot(list(correlations_lds[, , "2"], correlations_lds[, , "3"], correlations_lds[, , "4"], correlations_lds[, , "5"], correlations_lds[, , "6"]),
+  #     range = 0, # box whiskers to be the data extremes
+  #     xaxt = "n",
+  #     xlab = "Lead time",
+  #     ylab = bquote("Spearman" ~ rho)
+  #   )
+  #   for (i in 1:length(CI_bounds_boxplots[1, ])) {  ## CI_bounds_boxplots is internal data, created by function b
+  #     polygon(c(0, 6, 6, 0), c(rep(CI_bounds_boxplots[1, i], 2), rep(CI_bounds_boxplots[2, i], 2)), col = gray(0.8, alpha = 0.3))
+  #   }
+  #   Axis(side = 1, at = 1:6, labels = c(as.character(2:6), "all"))
+
+  # return(correlations_lds)
+}
---
+++ R/stability_test.R
@@ -0,0 +1,126 @@
+#' Test the model stability: 1. density plot
+#'
+#' We plot the density distribution of the different leadtimes using ggplot.
+#'
+#'
+#' @param ensemble The UNSEEN ensemble. This function expects an dataframe with the columns variable (precipitation) and leadtime.
+#' @param var_name The column name containing the variable to be analyzed. Defaults to "tprate".
+#' @param ld_name The column name containing the leadtimes. Defaults to "leadtime".
+#' @param lab The x-label. Defaults to the variable name (var_name).
+#'
+#' @return a plot showing the empirical probability density distribution for each leadtime
+#' @source Evaluation explained in more detail in Kelder et al. 2020
+#' @source Colorblind friendly palette  http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
+#' @export
+Model_stability_density <- function(ensemble, var_name = "tprate", ld_name = "leadtime", lab = var_name) {
+  # I select five colors and put black at the end.
+  cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#D55E00", "#000000") # , "#0072B2", "#CC79A7")
+  ### The Leadtime column has to be a factor for a grouped plot
+  ensemble[[ld_name]] <- as.factor(ensemble[[ld_name]])
+
+  p1 <-
+    ggplot2::ggplot(
+      data = ensemble,
+      mapping = ggplot2::aes_(
+        x = as.name(var_name), colour = as.name(ld_name))) +
+    # ggplot2::ggtitle("UK") +
+    ggplot2::labs(x = lab, y = "Density") +
+    ggplot2::geom_line(stat = "density") +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_colour_manual(values = cbPalette) #+
+  # theme(
+  #   text = element_text(size = 11),
+  #   axis.text = element_text(size = 11),
+  #   plot.title = element_text(hjust = 0.5)
+  # )
+
+  return(p1)
+}
+
+
+#' Test the model stability: 2. empirical extreme value plot
+#'
+#' We plot the density distribution of the different leadtimes using ggplot.
+#' We want to show the confidence interval of the distribution of all lead times pooled together,
+#' and test whether the individual lead times fall within these confidence intervals.
+#' Therefore we bootstrap the pooled leadtimes into series with equal length of the individual leadtimes (875), with n=10000.
+#'
+#' @param ensemble The UNSEEN ensemble. This function expects an dataframe with variables leadtime, precipitation.
+#' @param var_name The column name containing the variable to be analyzed. Defaults to "tprate".
+#' @param ld_name The column name containing the leadtimes. Defaults to "leadtime".
+#' @param lab The x-label. Defaults to the variable name (var_name).
+#'
+#' @return a plot with the empirical return values of the pooled ensemble including confidence intervals.
+#' Individual lead times are plotted on top.
+#' @source Evaluation explaned in more detail in Kelder et al. 2020
+#' @source Colorblind friendly palette  http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
+#' @export
+Model_stability_boot <- function(ensemble, var_name = "tprate", ld_name = "leadtime", lab = var_name) {
+  #Define necessary global variables
+  ci_2.5 <- ci_97.5 <- quantiles_all <- rps_all <- NULL
+
+  ### The Leadtime column has to be a factor for a grouped plot
+  ensemble[[ld_name]] <- as.factor(ensemble[[ld_name]])
+
+
+  ensemble_length <- length(ensemble[[ld_name]])
+  leadtime_length <- sum(ensemble[[ld_name]] == 2)
+  bootstrapped_series <- sample(ensemble[[var_name]], size = leadtime_length * 10000, replace = T) # bootstraps the series of length equal to each lead time (875) with n= 10.000
+  bootstrapped_array <- array(bootstrapped_series, dim = c(leadtime_length, 10000)) # Creates an array with 10.000 series of 875 values
+
+  rps <- ensemble_length / 1:ensemble_length # The return periods for the entire ensemble of 4,375‬ years
+  rps_ld <- leadtime_length / 1:leadtime_length # The return periods for the ensemble split up into 5 leadtimes, 875 years
+
+  Rvs <- apply(bootstrapped_array, MARGIN = 2, FUN = stats::quantile, probs = 1 - 1 / (rps)) # apply the function to each of the 10.000 series
+
+  # calculate the lower and upper interval from the 10.000 values for each quantile.
+  ci_rvs <- apply(Rvs, MARGIN = 1, FUN = stats::quantile, probs = c(0.025, 0.975))
+
+  ## Create a dataframe including the return periods, empirical values and confidence intervals
+  df_quantiles <- ensemble %>%
+    dplyr::mutate(rps_all = rps, quantiles_all = stats::quantile(!!as.name(var_name), 1 - 1 / (rps)))
+
+  df_quantiles <- df_quantiles %>%
+    dplyr::group_by(!!as.name(ld_name)) %>%
+    dplyr::mutate(rps_ld = rps_ld, quantiles_ld = stats::quantile(!!as.name(var_name), 1 - 1 / (rps_ld)))
+
+  df_quantiles$ci_2.5 <- ci_rvs[1, ]
+  df_quantiles$ci_97.5 <- ci_rvs[2, ]
+
+  # And plot
+  cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#D55E00", "#000000") # , "#0072B2", "#CC79A7")
+  cols <- c("95 % CI" = "black")
+  p2 <-
+    ggplot2::ggplot(df_quantiles) +
+    ggplot2::geom_line(ggplot2::aes(x = rps_all, y = quantiles_all)) +
+    ggplot2::geom_line(ggplot2::aes_(x = ~rps_ld, y = ~quantiles_ld, col = as.name(ld_name))) +
+    ggplot2::geom_ribbon(ggplot2::aes(x = rps_all, ymin = ci_2.5, ymax = ci_97.5, fill = "95 % CI"), alpha = 0.1) +
+    # xlim(NA,875)+
+    ggplot2::scale_x_log10(limits = c(NA, leadtime_length)) +
+    # scale_x_continuous(trans='log10') +
+    ggplot2::scale_fill_manual(name = "Pooled data", values = cols) +
+    ggplot2::scale_colour_manual(values = cbPalette) +
+    ggplot2::xlab("Return period (years)") +
+    ggplot2::ylab(lab) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "none",
+                   text = ggplot2::element_text(size = 11),
+                   axis.text = ggplot2::element_text(size = 11)
+    )
+  return(p2)
+}
+
+stability_test <- function(ensemble, var_name = "tprate", ld_name = "leadtime", lab = var_name) {
+  #combine plots from function 1 and 2
+  p1 <- Model_stability_density(ensemble = ensemble, var_name = var_name, ld_name = ld_name, lab = lab)
+  p2 <- Model_stability_boot(ensemble = ensemble, var_name = var_name, ld_name = ld_name, lab = lab)
+  p_combined <- ggpubr::ggarrange(p1, p2,
+                          labels = c("a", "b"), # , "c", "d"),
+                          hjust = c(-0.5, 1, -0.5, 1),
+                          ncol = 1, nrow = 2,
+                          font.label = list(size = 11, color = "black", face = "bold", family = NULL),
+                          common.legend = TRUE
+  )
+  return(p_combined)
+}
